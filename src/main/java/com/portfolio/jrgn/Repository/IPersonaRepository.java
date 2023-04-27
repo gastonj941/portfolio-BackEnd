74d1a8d07c4f7a630103c5c4c346933cb6d5969e
gaston <gastonj941@gmail.com>
2023-04-27 03:23:47 -0300

    commit inicial

---
+++ src/main/java/com/portfolio/jrgn/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.jrgn.Repository;
+
+import com.portfolio.jrgn.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long>{
+    
+}
